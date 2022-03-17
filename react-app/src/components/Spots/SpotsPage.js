c3e0ef7c9ee23d31ec168a5987e4b04c7b0312e3
vernfongchao <Vernchao93@gmail.com>
2022-03-17 09:13:09 -0700

    fixing heroku deplosy

--- react-app/src/components/Spots/SpotsPage.js
+++ react-app/src/components/Spots/SpotsPage.js
@@ -10,7 +10,7 @@ const SpotsPage = () => {
     // const [errorImage,setErrorImage] = useState("")
     
     const handleImage = (e) => {
-        e.target.src="../../../images/image-not-found-scaled.png"
+        e.target.src="../../../public/images/image-not-found-scaled.png"
         // setErrorImage("../../../images/image-not-found-scaled.png")
     }
 
