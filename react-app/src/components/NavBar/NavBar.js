136ddf46b7e8410d9c4af09f3c6bbd4de73fcee4
vernfongchao <Vernchao93@gmail.com>
2022-03-20 12:10:10 -0700

    saving to deploy

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-// import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileMenu from './ProfileMenu';
 import CreateSpotModal from '../Spots/CreateSpotModal';
@@ -12,6 +12,12 @@ const NavBar = () => {
 
   return (
     <nav className='navbar-container'>
+      <div>
+        <img src="../../images/Stay-The-Night-Logo.png">
+          <Link to='/spots'>
+          </Link>
+        </img>
+      </div>
 
       <div className='navbar-right-container'>
 
