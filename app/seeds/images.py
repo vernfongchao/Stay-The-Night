2b2b56e38173b7fa58fbf813ffe022c797c20c67
vernfongchao <Vernchao93@gmail.com>
2022-03-20 20:33:12 -0700

    more seeds added

--- app/seeds/images.py
+++ app/seeds/images.py
@@ -23,57 +23,68 @@ def seed_images():
         image="https://a0.muscache.com/im/pictures/miso/Hosting-48392852/original/86e70ba3-98e4-4551-9fd8-9e667719ea1e.jpeg?im_w=1440"
 
     )
+
     image6 = Image(
         spot_id=2,
-        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/80797805-86d8-41e7-ba6e-05bceb1f91f1.jpeg?im_w=1200'
+        image="https://a0.muscache.com/im/pictures/fa803223-80cd-41bf-ad2a-4aa314fd0425.jpg?im_w=1440"
     )
     image7 = Image(
-        spot_id=2,
-        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/1693f517-abc6-40f0-bfb2-1fb4c2437077.jpeg?im_w=1440'
+        spot_id=3,
+        image="https://a0.muscache.com/im/pictures/miso/Hosting-542081220811782043/original/c2fc78ba-fc0b-4c66-b413-fdf22f52666d.jpeg?im_w=1200"
     )
     image8 = Image(
-        spot_id=2,
-        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/16ca1e80-7083-442e-8328-37709016c1ce.jpeg?im_w=1440'
+        spot_id=4,
+        image="https://a0.muscache.com/im/pictures/miso/Hosting-53813776/original/5cd73400-c1e3-453d-8296-2a85927828f8.jpeg?im_w=1440"
     )
+
     image9 = Image(
-        spot_id=2,
-        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/1f898a2b-e8fd-4c21-bb96-502b19c4c5e7.jpeg?im_w=1440'
+        spot_id=5,
+        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/80797805-86d8-41e7-ba6e-05bceb1f91f1.jpeg?im_w=1200'
     )
     image10 = Image(
-        spot_id=2,
+        spot_id=5,
+        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/1693f517-abc6-40f0-bfb2-1fb4c2437077.jpeg?im_w=1440'
+    )
+    image11 = Image(
+        spot_id=5,
+        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/16ca1e80-7083-442e-8328-37709016c1ce.jpeg?im_w=1440'
+    )
+    image12 = Image(
+        spot_id=5,
+        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/1f898a2b-e8fd-4c21-bb96-502b19c4c5e7.jpeg?im_w=1440'
+    )
+    image13 = Image(
+        spot_id=5,
         image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/165ec554-7f10-4d9d-9390-4c5b54d34680.jpeg?im_w=1200'
     )
 
-    image11 = Image(
-        spot_id=3,
+    image14 = Image(
+        spot_id=6,
         image='https://a0.muscache.com/im/pictures/fda15318-534f-4d53-88f2-e6053c971ccc.jpg?im_w=1440'
     )
-    image12 = Image(
-        spot_id=3,
+    image15 = Image(
+        spot_id=6,
         image='https://a0.muscache.com/im/pictures/bc5774ec-57b5-43cf-864d-4d13eb3785a7.jpg?im_w=1200'
     )
 
-    image13 = Image(
-        spot_id=3,
+    image16 = Image(
+        spot_id=6,
         image='https://a0.muscache.com/im/pictures/c6bb8255-7be7-44f2-83f5-6a6f4220bcc7.jpg?im_w=1440'
     )
 
-    image14 = Image(
-        spot_id=3,
+    image17 = Image(
+        spot_id=6,
         image='https://a0.muscache.com/im/pictures/30939c5b-bbe3-4ed9-aea0-ae0c3ad0ca9d.jpg?im_w=1440'
     )
 
-    image15 = Image(
-        spot_id=3,
+    image18 = Image(
+        spot_id=6,
         image='https://a0.muscache.com/im/pictures/49dfbadf-1a05-459a-8c93-bce20ec4f56f.jpg?im_w=1440'
     )
 
-
-
-
-    db.session.add_all([image1,image2,image3,image4,image5])
-    db.session.add_all([image6,image7,image8,image9,image10])
-    db.session.add_all([image11,image12,image13,image14,image15])
+    db.session.add_all([image1, image2, image3, image4, image5])
+    db.session.add_all([image6, image7, image8, image9, image10])
+    db.session.add_all([image11, image12, image13, image14, image15,image16,image17,image18])
 
     db.session.commit()
 
--- app/seeds/spots.py
+++ app/seeds/spots.py
@@ -1,49 +1,89 @@
 from app.models import db, Spot
 
+
 def seed_spots():
     spot1 = Spot(
-        user_id=1, 
-        address="18453 Martin Ave", 
-        city="Homewood", 
+        user_id=1,
+        address="18453 Martin Ave",
+        city="Homewood",
         state="Illinois",
         country="USA",
-        name="2 BR Modern Queen Suite w/ Hot Tub & Outdoor Oasis", 
-        price= 249,
-        description= "A Rare gem 💎in the heart of Homewood. This Quaint 2-bedroom remodeled home is charming, sophisticated and discrete. It is minutes away from the Metra station, golf course and downtown Homewood shops. The energy is very calm and quiet ☯️. This is the home away from home where details are everything and the pictures do not capture the essence of this luxurious space. Experience the love, the joy and the peace that continues to leave its mark.",
-        guest = 4,
-        bathroom = 2,
-        bedroom= 2
+        name="2 BR Modern Queen Suite w/ Hot Tub & Outdoor Oasis",
+        price=249,
+        description="A Rare gem 💎in the heart of Homewood. This Quaint 2-bedroom remodeled home is charming, sophisticated and discrete. It is minutes away from the Metra station, golf course and downtown Homewood shops. The energy is very calm and quiet ☯️. This is the home away from home where details are everything and the pictures do not capture the essence of this luxurious space. Experience the love, the joy and the peace that continues to leave its mark.",
+        guest=4,
+        bathroom=2,
+        bedroom=2
     )
     spot2 = Spot(
-        user_id=2, 
-        address="1616 Battle Ln", 
-        city="Monroe", 
-        state="North Carolina",
+        user_id=1,
+        address="8845 Valley View St.",
+        city="Elgin",
+        state="Illinois",
         country="USA",
-        name="★The Black Chateau★ Springbreak| Heated Pool in Living Room!!! | ♛Royal Beds | Pool Table", 
-        price= 400,
-        description= "Enjoy this 1800's home located conveniently near Historic downtown Monroe, NC. Monroe features a variety of activities for anyone looking for a weekend away from the city of Charlotte, NC. Enjoy wine tasting at local vineyards, shop antiques on the famous “Treasure Hunt”, or grab dinner downtown. Built in 1842, The Browning home place was once an inn for weary travelers! Come stay in the oldest standing structure in Monroe and enjoy a piece of history.",
-        guest = 7,
-        bathroom = 4,
-        bedroom= 3
+        name="Charming Elgin Home with a Great Location",
+        price=117,
+        description="Come enjoy this beautifully restored and charming historic home from the early 1920s. Our place is good for couples, families or for a getaway with friends. Outdoor space with fully fenced backyard. This 2 bed 1 bath has been fully restored and is absolutely adorable! Walking distance to downtown Elgin (less than one mile) and Metra station (only one hour train ride into the city!), and less than 5 minute drive to I-90. Relax and get cozy in this adorable space.",
+        guest=5,
+        bathroom=1,
+        bedroom=2
     )
     spot3 = Spot(
-        user_id=3, 
-        address="221 Cherokee Dr", 
-        city="Barnesville", 
-        state="Georgia",
+        user_id=1,
+        address="84 Mayfield St.",
+        city="Richardson",
+        state="TX",
         country="USA",
-        name="The Farm House", 
-        price= 49,
-        description= "The Farm House is a primitive cottage and resides on 400 acres outside of Barnesville, Georgia. Bunn Ranch is a working cattle and sheep farm. This space is a two story primitive cottage with primitive artwork and a claw foot tub. Sit in your choice of antique rockers that have been collected over the years. The floors and stairs were salvaged from an old home that was here on the farm. Surrounded by rolling hills and close to town, come enjoy some time for YOU! We will consider STR students.",
-        guest = 5,
-        bathroom = 1,
-        bedroom= 4
+        name="⭑MODERN&CLEAN⭑ GREAT DEAL FOR TRAVEL WORK OR FUN",
+        price=109,
+        description="1 Bedroom, 1 Batheroom, with 2 75 inch TV's in bedroom and living room. A King size Bed with 6 pillows, A comfortable couch that can sleep a 3rd or 4th person. Kitchen has condiment, plates/bowls, spoon/forks, coffee machine, toaster, towels, toilet paper, q tips, lotion, a patio with furniture. Fridge has an Ice Maker machine. Laundry comes with detergent(You will have to buy more if you use up our own). Body wash, Shampoo/Conditioner is included(You will have to buy more if you use up our own). This space is designed for vacation and business travelers who are searching for a home away from home with the added amenities comparable to a 4 star hotel. Easy Self Check in, easy access from parking parking to home without a long walk, full gym, conference center and more.",
+        guest=4,
+        bathroom=1,
+        bedroom=1
     )
 
-    db.session.add(spot1)
-    db.session.add(spot2)
-    db.session.add(spot3)
+    spot4 = Spot(
+        user_id=1,
+        address="942 Edgewood Dr.",
+        city="Stow",
+        state="OH",
+        country="USA",
+        name="The Lakeside Cottage",
+        price=100,
+        description="Spend time at the relaxing newly renovated cottage on Meadowbrook Lake. Location is nearby Summit Metro Parks, Blossom Music Center, and Sarah's Vineyard. Short distance from Brandywine and Bostin Mills Ski Resorts. This cottage features a beautiful view of the lake. If you are a nature lover, you will appreciate the many windows to enhance the view. The home is walking distance to the Meadowbrook Lake fishing dock, basketball courts, and playground.",
+        guest=4,
+        bathroom=1,
+        bedroom=2
+    )
+
+    spot5 = Spot(
+        user_id=2,
+        address="1616 Battle Ln",
+        city="Monroe",
+        state="North Carolina",
+        country="USA",
+        name="★The Black Chateau★ Springbreak| Heated Pool in Living Room!!! | ♛Royal Beds | Pool Table",
+        price=400,
+        description="Enjoy this 1800's home located conveniently near Historic downtown Monroe, NC. Monroe features a variety of activities for anyone looking for a weekend away from the city of Charlotte, NC. Enjoy wine tasting at local vineyards, shop antiques on the famous “Treasure Hunt”, or grab dinner downtown. Built in 1842, The Browning home place was once an inn for weary travelers! Come stay in the oldest standing structure in Monroe and enjoy a piece of history.",
+        guest=7,
+        bathroom=4,
+        bedroom=3
+    )
+    spot6 = Spot(
+        user_id=3,
+        address="221 Cherokee Dr",
+        city="Barnesville",
+        state="Georgia",
+        country="USA",
+        name="The Farm House",
+        price=49,
+        description="The Farm House is a primitive cottage and resides on 400 acres outside of Barnesville, Georgia. Bunn Ranch is a working cattle and sheep farm. This space is a two story primitive cottage with primitive artwork and a claw foot tub. Sit in your choice of antique rockers that have been collected over the years. The floors and stairs were salvaged from an old home that was here on the farm. Surrounded by rolling hills and close to town, come enjoy some time for YOU! We will consider STR students.",
+        guest=5,
+        bathroom=1,
+        bedroom=4
+    )
+
+    db.session.add_all([spot1, spot2, spot3, spot4, spot5, spot6])
 
     db.session.commit()
 
