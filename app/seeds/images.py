e9f245dc4b0464a1cebfa7d6dd5a9c758b5af8b5
vernfongchao <Vernchao93@gmail.com>
2022-03-22 00:33:22 -0700

    changing seed file extention to match validation

--- app/seeds/images.py
+++ app/seeds/images.py
@@ -4,139 +4,145 @@ from app.models import db, Image
 def seed_images():
     image1 = Image(
         spot_id=1,
-        image='https://a0.muscache.com/im/pictures/miso/Hosting-48392852/original/6751716c-5812-40ed-95d7-027d6a033a6a.jpeg?im_w=1200'
+        image='https://a0.muscache.com/im/pictures/miso/Hosting-48392852/original/6751716c-5812-40ed-95d7-027d6a033a6a.jpeg'
     )
     image2 = Image(
         spot_id=1,
-        image='https://a0.muscache.com/im/pictures/miso/Hosting-48392852/original/c9bf8206-f853-47e0-b27d-be2422c6e313.jpeg?im_w=1200'
+        image='https://a0.muscache.com/im/pictures/miso/Hosting-48392852/original/c9bf8206-f853-47e0-b27d-be2422c6e313.jpeg'
     )
     image3 = Image(
         spot_id=1,
-        image='https://a0.muscache.com/im/pictures/miso/Hosting-48392852/original/78cf38b0-f76a-497e-a0b8-0fdd431d592d.jpeg?im_w=1200'
+        image='https://a0.muscache.com/im/pictures/miso/Hosting-48392852/original/78cf38b0-f76a-497e-a0b8-0fdd431d592d.jpeg'
     )
     image4 = Image(
         spot_id=1,
-        image='https://a0.muscache.com/im/pictures/miso/Hosting-48392852/original/98347eef-7b60-440d-8d7c-bb14767419c3.jpeg?im_w=1440'
+        image='https://a0.muscache.com/im/pictures/miso/Hosting-48392852/original/98347eef-7b60-440d-8d7c-bb14767419c3.jpeg'
     )
     image5 = Image(
         spot_id=1,
-        image="https://a0.muscache.com/im/pictures/miso/Hosting-48392852/original/86e70ba3-98e4-4551-9fd8-9e667719ea1e.jpeg?im_w=1440"
+        image="https://a0.muscache.com/im/pictures/miso/Hosting-48392852/original/86e70ba3-98e4-4551-9fd8-9e667719ea1e.jpeg"
 
     )
     image6 = Image(
         spot_id=2,
-        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/80797805-86d8-41e7-ba6e-05bceb1f91f1.jpeg?im_w=1200'
+        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/80797805-86d8-41e7-ba6e-05bceb1f91f1.jpeg'
     )
     image7 = Image(
         spot_id=2,
-        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/1693f517-abc6-40f0-bfb2-1fb4c2437077.jpeg?im_w=1440'
+        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/1693f517-abc6-40f0-bfb2-1fb4c2437077.jpeg'
     )
     image8 = Image(
         spot_id=2,
-        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/16ca1e80-7083-442e-8328-37709016c1ce.jpeg?im_w=1440'
+        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/16ca1e80-7083-442e-8328-37709016c1ce.jpeg'
     )
     image9 = Image(
         spot_id=2,
-        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/1f898a2b-e8fd-4c21-bb96-502b19c4c5e7.jpeg?im_w=1440'
+        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/1f898a2b-e8fd-4c21-bb96-502b19c4c5e7.jpeg'
     )
     image10 = Image(
         spot_id=2,
-        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/165ec554-7f10-4d9d-9390-4c5b54d34680.jpeg?im_w=1200'
+        image='https://a0.muscache.com/im/pictures/prohost-api/Hosting-46153431/original/165ec554-7f10-4d9d-9390-4c5b54d34680.jpeg'
     )
 
     image11 = Image(
         spot_id=3,
-        image='https://a0.muscache.com/im/pictures/fda15318-534f-4d53-88f2-e6053c971ccc.jpg?im_w=1440'
+        image='https://a0.muscache.com/im/pictures/fda15318-534f-4d53-88f2-e6053c971ccc.jpg'
     )
     image12 = Image(
         spot_id=3,
-        image='https://a0.muscache.com/im/pictures/bc5774ec-57b5-43cf-864d-4d13eb3785a7.jpg?im_w=1200'
+        image='https://a0.muscache.com/im/pictures/bc5774ec-57b5-43cf-864d-4d13eb3785a7.jpg'
     )
 
     image13 = Image(
         spot_id=3,
-        image='https://a0.muscache.com/im/pictures/c6bb8255-7be7-44f2-83f5-6a6f4220bcc7.jpg?im_w=1440'
+        image='https://a0.muscache.com/im/pictures/c6bb8255-7be7-44f2-83f5-6a6f4220bcc7.jpg'
     )
 
     image14 = Image(
         spot_id=3,
-        image='https://a0.muscache.com/im/pictures/30939c5b-bbe3-4ed9-aea0-ae0c3ad0ca9d.jpg?im_w=1440'
+        image='https://a0.muscache.com/im/pictures/30939c5b-bbe3-4ed9-aea0-ae0c3ad0ca9d.jpg'
     )
 
     image15 = Image(
         spot_id=3,
-        image='https://a0.muscache.com/im/pictures/49dfbadf-1a05-459a-8c93-bce20ec4f56f.jpg?im_w=1440'
+        image='https://a0.muscache.com/im/pictures/49dfbadf-1a05-459a-8c93-bce20ec4f56f.jpg'
     )
 
     image16 = Image(
         spot_id=4,
-        image="https://a0.muscache.com/im/pictures/fa803223-80cd-41bf-ad2a-4aa314fd0425.jpg?im_w=1440"
+        image="https://a0.muscache.com/im/pictures/fa803223-80cd-41bf-ad2a-4aa314fd0425.jpg"
     )
 
     image17 = Image(
         spot_id=4,
-        image="https://a0.muscache.com/im/pictures/2d7ea8a9-ddc6-48ef-ba98-049281d710fa.jpg?im_w=1440"
+        image="https://a0.muscache.com/im/pictures/2d7ea8a9-ddc6-48ef-ba98-049281d710fa.jpg"
     )
     image18 = Image(
         spot_id=4,
-        image="https://a0.muscache.com/im/pictures/c163228c-17d7-4cfb-adaf-de7dccc9828d.jpg?im_w=1440"
+        image="https://a0.muscache.com/im/pictures/c163228c-17d7-4cfb-adaf-de7dccc9828d.jpg"
     )
     image19 = Image(
         spot_id=4,
-        image="https://a0.muscache.com/im/pictures/20428785-dd33-4ccb-b58a-cea5bc335398.jpg?im_w=1440"
+        image="https://a0.muscache.com/im/pictures/20428785-dd33-4ccb-b58a-cea5bc335398.jpg"
     )
 
     image20 = Image(
         spot_id=4,
-        image="https://a0.muscache.com/im/pictures/71407322-4c79-4ddb-a8e6-7f52a1fbaa3b.jpg?im_w=1440"
+        image="https://a0.muscache.com/im/pictures/71407322-4c79-4ddb-a8e6-7f52a1fbaa3b.jpg"
     )
+
+
+
 
     image21 = Image(
         spot_id=5,
-        image="https://a0.muscache.com/im/pictures/miso/Hosting-542081220811782043/original/c2fc78ba-fc0b-4c66-b413-fdf22f52666d.jpeg?im_w=1200"
+        image="https://a0.muscache.com/im/pictures/miso/Hosting-542081220811782043/original/c2fc78ba-fc0b-4c66-b413-fdf22f52666d.jpeg"
     )
-
+    
     image22 = Image(
         spot_id=5,
-        image="https://a0.muscache.com/im/pictures/miso/Hosting-542081220811782043/original/6e5a8f0b-b92b-4e12-8f27-2d2b029301f4.jpeg?im_w=1440"
+        image="https://a0.muscache.com/im/pictures/miso/Hosting-542081220811782043/original/6e5a8f0b-b92b-4e12-8f27-2d2b029301f4.jpeg"
     )
-
+    
     image23 = Image(
         spot_id=5,
-        image="https://a0.muscache.com/im/pictures/miso/Hosting-542081220811782043/original/d32b0b67-1a6d-475a-a86b-da5f878a9966.jpeg?im_w=1440"
+        image="https://a0.muscache.com/im/pictures/miso/Hosting-542081220811782043/original/d32b0b67-1a6d-475a-a86b-da5f878a9966.jpeg"
     )
-
+    
     image24 = Image(
         spot_id=5,
-        image="https://a0.muscache.com/im/pictures/miso/Hosting-542081220811782043/original/61dc72cd-cf51-401d-a5da-cdc5734c0b70.jpeg?im_w=1440"
+        image="https://a0.muscache.com/im/pictures/miso/Hosting-542081220811782043/original/61dc72cd-cf51-401d-a5da-cdc5734c0b70.jpeg"
     )
 
+        
     image25 = Image(
         spot_id=5,
-        image="https://a0.muscache.com/im/pictures/miso/Hosting-542081220811782043/original/d187b539-7e9c-4adb-a462-15935f4e9ad4.jpeg?im_w=1440"
+        image="https://a0.muscache.com/im/pictures/miso/Hosting-542081220811782043/original/d187b539-7e9c-4adb-a462-15935f4e9ad4.jpeg"
     )
+
 
     image26 = Image(
         spot_id=6,
-        image="https://a0.muscache.com/im/pictures/miso/Hosting-53813776/original/5cd73400-c1e3-453d-8296-2a85927828f8.jpeg?im_w=1440"
+        image="https://a0.muscache.com/im/pictures/miso/Hosting-53813776/original/5cd73400-c1e3-453d-8296-2a85927828f8.jpeg"
     )
     image27 = Image(
         spot_id=6,
-        image="https://a0.muscache.com/im/pictures/b63274be-59d7-4742-acff-e07d6344d788.jpg?im_w=1200"
+        image="https://a0.muscache.com/im/pictures/b63274be-59d7-4742-acff-e07d6344d788.jpg"
     )
     image28 = Image(
         spot_id=6,
-        image="https://a0.muscache.com/im/pictures/b51650db-d322-47de-915a-cfa007162654.jpg?im_w=1440"
+        image="https://a0.muscache.com/im/pictures/b51650db-d322-47de-915a-cfa007162654.jpg"
     )
     image29 = Image(
         spot_id=6,
-        image="https://a0.muscache.com/im/pictures/85aa6290-6c8e-4af2-b91c-e22ffe8a311c.jpg?im_w=1440"
+        image="https://a0.muscache.com/im/pictures/85aa6290-6c8e-4af2-b91c-e22ffe8a311c.jpg"
     )
 
     image30 = Image(
         spot_id=6,
-        image="https://a0.muscache.com/im/pictures/ebc69a04-4db4-4409-ad3f-c37f6259cf0d.jpg?im_w=1440"
+        image="https://a0.muscache.com/im/pictures/ebc69a04-4db4-4409-ad3f-c37f6259cf0d.jpg"
     )
+
 
     db.session.add_all([image1, image2, image3, image4, image5])
     db.session.add_all([image6, image7, image8, image9, image10])
