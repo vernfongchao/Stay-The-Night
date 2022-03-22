d53c3b66993765c77309525a996599eef6f47339
vernfongchao <Vernchao93@gmail.com>
2022-03-22 01:08:46 -0700

    changed location of flask form so errors line up in order

--- app/forms/spot_form.py
+++ app/forms/spot_form.py
@@ -5,6 +5,8 @@ from wtforms.validators import DataRequired, NumberRange, Length
 
 class SpotForm(FlaskForm):
     user_id = IntegerField("user_id", validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired(), Length(
+        min=1, max=100, message='Name must be at least than 1 characters and no more than 100 characters')])
     address = StringField('address', validators=[DataRequired(), Length(
         min=1, max=50, message='Address must be at least than 1 characters and no more than 50 characters')])
     city = StringField('city', validators=[DataRequired(), Length(
@@ -13,12 +15,10 @@ class SpotForm(FlaskForm):
         min=1, max=50, message='State must be at least than 1 characters and no more than 50 characters')])
     country = StringField('country', validators=[DataRequired(), Length(
         min=1, max=50, message='Country must be at least than 1 characters and no more than 50 characters')])
-    name = StringField('name', validators=[DataRequired(), Length(
-        min=1, max=100, message='Name must be at least than 1 characters and no more than 100 characters')])
-    price = IntegerField('price', validators=[DataRequired(), NumberRange(
-        min=1, max=1000000, message='Number must be between 1 and 1000000')])
     description = StringField('description', validators=[DataRequired(), Length(
         min=1, max=1000, message='Description name must be at least than 1 characters and no more than 1000 characters')])
+    price = IntegerField('price', validators=[DataRequired(), NumberRange(
+        min=1, max=1000000, message='Number must be between 1 and 1000000')])
     guest = IntegerField('guests', validators=[DataRequired(), NumberRange(
         min=1, max=1000, message='Number must be between 1 and 1000')])
     bedroom = IntegerField('bedrooms', validators=[DataRequired(), NumberRange(
