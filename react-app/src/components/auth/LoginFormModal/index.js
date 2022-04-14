4b29c81d74da93d1a43c2ce3c2ac5b98cb8de803
vernfongchao <Vernchao93@gmail.com>
2022-04-13 20:21:49 -0700

    changed ux on profile menu

--- react-app/src/components/auth/LoginFormModal/index.js
+++ react-app/src/components/auth/LoginFormModal/index.js
@@ -1,18 +1,24 @@
-import React, {useState} from "react"
-import {Modal} from '../../../context/Modal'
+import React, { useState } from "react"
+import { Modal } from '../../../context/Modal'
 import LoginForm from "./LoginForm"
 
-const LoginFormModal = ({setShowMenu}) => {
-    const [showModal,setShowModal] = useState(false)
+const LoginFormModal = ({ setShowMenu}) => {
+    const [showModal, setShowModal] = useState(false)
 
     return (
-        <div className= "login-form-modal-container">
-            <div className="login-form-button-container" onClick={() => setShowModal(true)}>
+        <div className="login-form-modal-container">
+            <div className="login-form-button-container"
+                onClick={() => {
+                    setShowModal(true)
+                }}>
                 <p className="login-form-button-text">Log in</p>
             </div>
             {showModal && (
-                <Modal onClose={()=> setShowModal(false)}>
-                    <LoginForm setShowMenu={setShowMenu}/>
+                <Modal onClose={() => {
+                    setShowModal(false)
+                    setShowMenu(false)}
+                    }>
+                    <LoginForm setShowMenu={setShowMenu} />
                 </Modal>
             )}
         </div>
