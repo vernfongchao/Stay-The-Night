a5871dfdafd9c94406a03abe44199c87dd1b395b
vernfongchao <Vernchao93@gmail.com>
2022-04-16 13:48:49 -0700

    styled favorites page

--- react-app/src/components/Profiles/MyFavorites/MyFavorites.js
+++ react-app/src/components/Profiles/MyFavorites/MyFavorites.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import './MyFavorites.css'
+
 const MyFavorites = () => {
     return(
-        <div>
+        <div className='favorites-spots-page-container'>
             <h1>Feature Coming Soon TM!</h1>
         </div>
     )
