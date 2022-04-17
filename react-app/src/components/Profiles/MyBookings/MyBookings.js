47e874243c9c7d0cf5fea61676b9cf1705777445
vernfongchao <Vernchao93@gmail.com>
2022-04-17 13:25:09 -0700

    sorted arrays so earlier Oncoming bookings comes first, and last ended bookings comes first

--- react-app/src/components/Profiles/MyBookings/MyBookings.js
+++ react-app/src/components/Profiles/MyBookings/MyBookings.js
@@ -15,11 +15,16 @@ const MyBookings = () => {
     const spots = useSelector(state => state.spots)
     const bookings = Object.values(useSelector(state => state.bookings)).reverse()
 
+    const pastBookings = bookings?.filter(({ start_date }) => moment(start_date).utcOffset(7) <= moment().utcOffset(7)).sort((a, b) => moment(a.end_date).valueOf() > moment(b.end_date).valueOf())
+    const futureBookings = bookings?.filter(({ start_date }) => moment(start_date).utcOffset(7) > moment().utcOffset(7)).sort((a, b) => moment(a.start_date).valueOf() - moment(b.start_date).valueOf())
 
     if (user?.id !== +id) {
         history.push('/403-Unauthorized')
     }
 
+    const handleImage = (e) => {
+        e.target.src = "../../../../static/house1.jpg"
+    }
 
     useEffect(() => {
         window.scrollTo(0, 0)
@@ -30,10 +35,13 @@ const MyBookings = () => {
             <div className="my-bookings-header-container">
                 {bookings.length ? <h1>My Bookings</h1> : <h1>No Bookings Made</h1>}
             </div>
-            {bookings &&
+            <div>
+                {futureBookings.length ? <h1>Oncoming Bookings</h1> : null}
+            </div>
+            {futureBookings &&
                 <div className="my-all-spots-container">
                     {
-                        bookings.map((booking) => (
+                        futureBookings.map((booking) => (
                             <div className="my-each-spot-container" key={booking.id}>
                                 <EditDeleteBookingModal booking={booking} />
                                 <Link to={`/spots/${spots[booking.spot_id].id}`}>
@@ -41,7 +49,37 @@ const MyBookings = () => {
                                         className="my-each-spot-image"
                                         src={spots[booking.spot_id].images[0].image}
                                         alt={`spots #${booking.spot_id}`}
-                                    // onError={handleImage}
+                                        onError={handleImage}
+                                    >
+                                    </img>
+                                </Link>
+                                <div className="my-spots-basic-container">
+                                    <div>
+                                        <span>Start Date {moment(booking.start_date).utcOffset(7).format('MMMM D YYYY')}</span>
+                                    </div>
+                                    <div>
+                                        <span>End Date {moment(booking.end_date).utcOffset(7).format('MMMM D YYYY')}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            }
+            <div className="my-bookings-past-header">
+                {pastBookings.length ? <h1>Past & On-Going Bookings</h1> : null}
+            </div>
+            {pastBookings &&
+                <div className="my-all-spots-container">
+                    {
+                        pastBookings.map((booking) => (
+                            <div className="my-each-spot-container" key={booking.id}>
+                                <Link to={`/spots/${spots[booking.spot_id].id}`}>
+                                    <img
+                                        className="my-each-spot-image"
+                                        src={spots[booking.spot_id].images[0].image}
+                                        alt={`spots #${booking.spot_id}`}
+                                        onError={handleImage}
                                     >
                                     </img>
                                 </Link>
